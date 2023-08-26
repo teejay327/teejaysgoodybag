b727f981c56939ab3fa19b729a51979fb6be95da
teejay327 <mrenergy@hotmail.com>
2023-08-26 15:32:49 +1000

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>TeeJay's goody-bag</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
