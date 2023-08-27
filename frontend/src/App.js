43c6b99b7aa0e464c0ff0721265ded785849b107
teejay327 <mrenergy@hotmail.com>
2023-08-27 23:12:14 +1000

    Added Header and Navbar in Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
+import { Container } from 'react-bootstrap';
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>TeeJay's goody-bag</h1>
-      </header>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>TeeJay's goody-bag</h1>
+        </Container>
+      </main>
     </div>
   );
 }
