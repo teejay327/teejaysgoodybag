f485815d6985f3c3c0a149354c7486ea7fd48bb4
teejay327 <mrenergy@hotmail.com>
2023-09-04 23:15:26 +1000

    Added React router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 import Header from "./components/Header";
 import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen';
+
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <h1>TeeJay's Goody Bag</h1>
-          <HomeScreen/>
+          <Outlet />
         </Container>
       </main>
       <Footer />
