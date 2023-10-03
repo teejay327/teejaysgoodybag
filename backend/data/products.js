0887d6fd6400485d5257dafac52fad6b96aabd36
teejay327 <mrenergy@hotmail.com>
2023-10-03 23:06:07 +1000

    Changed products.js list

--- backend/data/products.js
+++ backend/data/products.js
@@ -7,7 +7,7 @@ const products = [
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
     brand: 'Apple',
     category: 'Electronics',
-    price: 99.99,
+    price: 89.99,
     countInStock: 10,
     rating: 4.5,
     numReviews: 12,
