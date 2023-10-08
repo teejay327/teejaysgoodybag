49fc819d126973b022f74025bc9f31dc33965e2a
teejay327 <mrenergy@hotmail.com>
2023-10-09 00:36:08 +1000

    Added config file for mongoose

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
     process.exit(1);
   }
 };
+
+export default connectDB;
