4389c2e950877195f658f28b4e97725194eb6497
teejay327 <mrenergy@hotmail.com>
2023-10-12 23:30:09 +1000

    Added seeder file and scripts in package.json

--- backend/seeder.js
+++ backend/seeder.js
@@ -16,7 +16,6 @@ const importData = async() => {
   try {
     await Order.deleteMany();
     await Product.deleteMany();
-
     await User.deleteMany();
 
     const createdUsers = await User.insertMany(users);
@@ -34,4 +33,24 @@ const importData = async() => {
     console.error(`${error}`.red.inverse);
     process.exit(1);
   }
+};
+
+const destroyData = async() => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log('Data destroyed!'.red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === '-d') {
+  destroyData();
+} else {
+  importData();
 }
