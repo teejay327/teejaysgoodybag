9d31011d5761911a68c94613e2b0a2b7c0c4260c
teejay327 <mrenergy@hotmail.com>
2023-10-23 23:54:58 +1000

    added middleware to find errors

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -12,10 +12,12 @@ router.get('/', asyncHandler(async (req,res) => {
 router.get('/:id', asyncHandler(async (req,res) => {
   //const product = products.find((p) => p._id === req.params.id);
   const product = await Product.findById(req.params.id);
-  if (product) {
-    return res.json(product);
-  }
-  res.status(404).json({ message: 'Product not found' });
-}));
+    if (product) {
+      return res.json(product);
+    } else {
+     throw new Error('Resource not found');
+    }
+  })
+);
 
 export default router;
