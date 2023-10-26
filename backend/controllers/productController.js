818a3e8f62b87a55aaf00e3e017f01a43cead9fe
teejay327 <mrenergy@hotmail.com>
2023-10-26 23:49:54 +1000

    Added controller for routes

---
+++ backend/controllers/productController.js
@@ -0,0 +1,24 @@
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/productModel.js';
+
+// @desc Fetches all products
+// @route GET /api/products
+// @access Public
+const getProducts = asyncHandler(async (req,res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+// @desc Fetches a product
+// @route GET /api/products/:id
+// @access Public
+const getProductById = asyncHandler(async (req,res) => {
+  const product = await Product.findById(req.params.id);
+    if (product) {
+      return res.json(product);
+    } else {
+     throw new Error('Resource not found');
+    }
+});
+
+export default { getProducts, getProductById };
