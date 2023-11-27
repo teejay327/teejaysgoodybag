64ddfa9b213354e1b577a2c0cef9a9c68455463f
teejay327 <mrenergy@hotmail.com>
2023-11-27 23:57:48 +1000

    Added shopping cart functionality

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -6,7 +6,19 @@ const initialState = localStorage.getItem("cart") ? JSON.parse
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {}
+  reducers: {
+    addToCart: (state,action) => {
+      const item = action.payload;
+      const existItem = state.cartItems.find((x) => {
+        x._id === item._id;
+      });
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) => {
+          x._id === existItem._id ? item : x
+        });
+      }
+    }
+  }
 });
 
 export default cartSlice.reducer;
