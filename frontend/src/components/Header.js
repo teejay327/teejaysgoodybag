2770e3b7912b682757a41f35383f26e854dae6cc
teejay327 <mrenergy@hotmail.com>
2023-12-16 23:23:56 +1000

    Added cart items functionality in the header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,12 @@
-import { Navbar, Nav, Container, NavbarBrand } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  console.log(cartItems);
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -14,7 +18,18 @@ const Header = () => {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className='ms-auto'>
               <LinkContainer to='/cart'>
-                <Nav.Link ><FaShoppingCart />Cart</Nav.Link>
+                <Nav.Link >
+                  <FaShoppingCart />Cart
+                    {
+                      cartItems.length > 0 && (
+                        <Badge pill bg='success' style={{ marginLeft: "5px" }}>
+                          { cartItems.reduce((acc,item) => {
+                            return acc + item.qty
+                          },0)}
+                        </Badge>
+                      )
+                    }
+                  </Nav.Link>
               </LinkContainer>
               <LinkContainer to='/login'>
                 <Nav.Link href="/login"><FaUser />Sign In</Nav.Link>
