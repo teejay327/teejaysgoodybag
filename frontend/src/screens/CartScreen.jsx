0a9dafc295df370f272db031942f94c5b3e754e3
teejay327 <mrenergy@hotmail.com>
2023-12-19 00:30:21 +1000

    Added more functionality to Cartscreen

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -7,13 +7,28 @@ import Message from '../components/Message';
 
 
 const CartScreen = () => {
-const navigate = useNavigate();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
 
   return (
-    <div>
-
-    </div>
+    <Row>
+      <Col md={8}>
+        <h1 style={{ marginBottom: '20px' }}>Shopping Cart</h1>
+        { cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'>Go back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            Items
+          </ListGroup>
+        ) }
+      </Col>
+    </Row>
   )
-}
+};
 
 export default CartScreen;
