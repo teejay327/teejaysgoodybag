72710c7a514e72e1334065fc67f3ed4d6cad50d3
teejay327 <mrenergy@hotmail.com>
2023-12-21 00:20:06 +1000

    More building UI on shopping cart page

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -24,16 +24,48 @@ const CartScreen = () => {
         ) : (
           <ListGroup variant='flush'>
             { cartItems.map((item) => {
-              <ListGroup.Item key={ item._id }>
-                <Row>
-                  <Col md={2}>
-                    <Image src={ item.image } alt={ item.name } fluid rounded />
-                  </Col>
-                </Row>
-              </ListGroup.Item>
+              return (
+                <ListGroup.Item key={ item._id }>
+                  <Row>
+                    <Col md={2}>
+                      <Image src={ item.image } alt={ item.name } fluid rounded />
+                    </Col>
+                    <Col md={3}>
+                      <Link to={ `product/${ item._id }`} >{ item.name }</Link>
+                    </Col>
+                    <Col md={ 2 }>
+                      ${ item.price }
+                    </Col>
+                    <Col md={2}>
+
+                    <Form.Control
+                      as='select'
+                      value={ item.qty }
+                      onChange={(e) => { } }
+                      >
+                      {[...Array(item.countInStock).keys()].map((x) => {
+                        return <option key={x + 1} value={x + 1}>
+                          { x + 1 }
+                        </option>
+                      }) }
+                    </Form.Control>
+                    </Col>
+                    <Col md={2}>
+                      <Button type='button' variant='light' >
+                        <FaTrash />
+                      </Button >
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              )
             })}
           </ListGroup>
         ) }
+      </Col>
+      <Col md={4} >
+        <Card>
+          
+        </Card>
       </Col>
     </Row>
   )
