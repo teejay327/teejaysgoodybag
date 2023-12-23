090bc1fe247e59ff9c96e4513f31a0f513f5427e
teejay327 <mrenergy@hotmail.com>
2023-12-23 17:07:14 +1000

    Added remove from cart functionality

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
 import { FaTrash } from 'react-icons/fa';
 import Message from '../components/Message';
-import { addToCart } from '../slices/cartSlice';
+import { addToCart, removeFromCart } from '../slices/cartSlice';
 
 const CartScreen = () => {
   const navigate = useNavigate();
@@ -14,7 +14,15 @@ const CartScreen = () => {
   const { cartItems } = cart;
 
   const addToCartHandler = async (product,qty) => {
-    dispatch(addToCart({...product,qty}))
+    dispatch(addToCart({...product,qty}));
+  }
+
+  const removeFromCartHandler = async (id) => {
+    dispatch(removeFromCart(id));
+  }
+
+  const checkoutHandler = () => {
+    navigate('/login?redirect=/shipping');
   }
 
   return (
@@ -55,7 +63,9 @@ const CartScreen = () => {
                     </Form.Control>
                     </Col>
                     <Col md={2}>
-                      <Button type='button' variant='light' >
+                      <Button type='button' variant='light' 
+                        onClick={ () => 
+                          removeFromCartHandler(item._id) } >
                         <FaTrash />
                       </Button >
                     </Col>
@@ -81,7 +91,9 @@ const CartScreen = () => {
             </ListGroup.Item>
             <ListGroup.Item>
               <Button type='button' className='btn-block' 
-                disabled={ cartItems.length === 0 }>
+                disabled={ cartItems.length === 0 }
+                onClick={ checkoutHandler }
+                >
                 Proceed to Checkout
               </Button>
             </ListGroup.Item>
