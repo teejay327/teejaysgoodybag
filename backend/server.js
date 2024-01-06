5566bf51d4e78d3ff76a146bce1351c6a69178ae
teejay327 <mrenergy@hotmail.com>
2024-01-07 00:25:27 +1000

    Added middleware in userRoutes.js

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,11 @@ const port = 5000;
 connectDB();
 
 const app = express();
+
+// body parser for middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req,res) => {
   res.send('API is running ...');
  });
