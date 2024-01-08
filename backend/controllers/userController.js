cb868a73468cd292970747e9c44a167c8515185c
teejay327 <mrenergy@hotmail.com>
2024-01-08 22:49:30 +1000

    added email and password authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,20 @@ import User from '../models/userModel.js';
 // @route POST /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req,res) => {
-  res.send ('log user in');
+  const { email, password } = req.body; 
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    })
+  } else {
+    res.status(401);
+    throw new Error('invalid email or password');
+  }
 });
 
 // @desc Register user
