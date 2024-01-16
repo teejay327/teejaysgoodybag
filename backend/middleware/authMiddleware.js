b0c848573659f90cd04de3663ac10a6251fbe9b7
teejay327 <mrenergy@hotmail.com>
2024-01-16 23:54:17 +1000

    Added authorisation middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,38 @@
-import { jwt } from "jsonwebtoken";
-import asyncHandler  from "./asyncHandler";
+import jwt from "jsonwebtoken";
+import asyncHandler  from './asyncHandler';
 import User from '../models/userModel';
 
 // Protect routes
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  // read the jwt from the cookie
+  token = req.cookies.jwt;
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findByID(decoded.userId).select('-password');
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error('Not authorised, token failed');
+    }
+  } else {
+    res.status(401);
+    throw new Error('Not authorised, no token found');
+  }
+});
+
+// Admin middleware
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorised as admin');   
+  }
+}
+
+export { protect, admin };
