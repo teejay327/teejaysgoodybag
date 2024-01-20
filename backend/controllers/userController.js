2afaf4474681b020884d0917cfec7eae3499bfea
teejay327 <mrenergy@hotmail.com>
2024-01-21 00:16:44 +1000

    Completed registering the user and encrytping the password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
-import jwt from 'jsonwebtoken';
+import generateToken from '../utils/generateToken.js';
 
 // @desc Authorise user
 // @route POST /api/users/login
@@ -9,17 +9,8 @@ const loginUser = asyncHandler(async (req,res) => {
   const { email, password } = req.body; 
   const user = await User.findOne({ email });
 
-  if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30d'
-    });
-
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 60 * 1000  // 30 days
-    })
+ if (user && (await user.matchPassword(password))) {
+  generateToken(res, user._id);
 
     res.json({
       _id: user._id,
@@ -45,13 +36,15 @@ const registerUser = asyncHandler(async (req,res) => {
     throw new Error('User already exists');
   }
 
-  const user = await User.Create({ 
+  const user = await User.create({ 
     name,
     email,
     password
   });
 
   if (user) {
+    generateToken(res, user._id);
+
     res.status(201).json({
       _id: user._id,
       name: user.name,
